4f57fd4c1d6b9538c16b8cb1510117fc48b5aac6
Gaius Okoase <gaiusonosetale@gmail.com>
2025-09-07 16:06:26 +0100

    Add LEFT JOIN query for properties and reviews in joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,3 +2,9 @@ SELECT * FROM
     Bookings b
 INNER JOIN 
     Users u ON b.user_id = u.user_id;
+
+-- LEFT JOIN
+SELECT * FROM
+    properties p
+LEFT JOIN
+    reviews r ON p.property_id = r.property_id;
